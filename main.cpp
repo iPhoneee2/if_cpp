2e699c9f062c8f5fd1e3cb72b1724c94aadbc40b
iPhoneee2 <iphoneee.13@mail.ru>
2026-01-28 16:10:22 +0500

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    int i;
+    cin >> i;
+    if (i>0){
+        cout << i;
+    }
+    else {
+        cout << 0;
+    }
+    return 0;
+}
